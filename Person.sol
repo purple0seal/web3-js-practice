a4212386597998bd423be3b5b9c495ca372321c4
boungkwan.kim <naver@AL01240880.local>
2017-12-31 16:38:45 +0900

    Add modifier feature

--- Person.sol
+++ Person.sol
@@ -3,13 +3,23 @@ pragma solidity ^0.4.18;
 contract Person {
     string name;
     uint age;
+    address owner;
+    
+    function Person() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
     
     event Instructor(
         string name,
         uint age
     );
     
-    function setInstructor(string n, uint a) public {
+    function setInstructor(string n, uint a) onlyOwner public {
         name = n;
         age = a;
         
