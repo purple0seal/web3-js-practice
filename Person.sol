fd49396b15e2e615a84aae4ea0d8c0fcf779eb20
NAVER <naver@AL01240880.local>
2017-12-31 16:23:56 +0900

    Implement event feature

--- Person.sol
+++ Person.sol
@@ -4,9 +4,16 @@ contract Person {
     string name;
     uint age;
     
+    event Instructor(
+        string name,
+        uint age
+    );
+    
     function setInstructor(string n, uint a) public {
         name = n;
         age = a;
+        
+        Instructor(n, a);
     }
     
     function getInstructor() public constant returns (string, uint) {
